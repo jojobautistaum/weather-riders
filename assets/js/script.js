4e3a5285d07cb8918ed865190ae87f7b1e810d4b
Jojo Bautista <gjojob@yahoo.com>
2021-11-17 20:27:23 -0600

    Adding weather API request and log data we needed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+var city = "Bloomington MN";
+
+
 var getTransitApi = function() {
     var requestUrl = 'https://svc.metrotransit.org/NexTrip/11167?format=json';
   
@@ -20,4 +23,59 @@ var getTransitApi = function() {
         routeInfo(2);
     })
 }
+
+
+// Find the city
+function findCity(city) {
+    var locationUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=b7609117c1b58fc397022fe7414e5f44`;
+    
+    fetch(locationUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                if(data.length){
+                    console.log("Latitude: " + data[0].lat);
+                    console.log("Longitude: " + data[0].lon);
+                    checkWeather(data[0].lat, data[0].lon);
+                } else {
+                    alert("The city location is unknown: " + city);
+                }
+            });
+        }
+        else {
+            alert("Error: " + response.statusText);
+        }
+    })
+    .catch(function(error){
+        alert("Unable to connect to Open Weather. - " + error);
+    });
+}
+
+// Pull the weather info
+function checkWeather (latitude, longitude) {
+    var weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&limit=5&appid=b7609117c1b58fc397022fe7414e5f44`;
+    
+    fetch(weatherUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                var now = new Date(data.current.dt * 1000).toLocaleString("en-US",{month: "2-digit", day: "2-digit", year: "numeric", hour: "2-digit", minute: "2-digit"});
+                console.log("Today:" + now);
+                console.log("Weather Icon: " + data.current.weather[0].icon);
+                console.log("Weather Description: " + data.current.weather[0].description);
+                console.log("Temperature: " + data.current.temp + " \xB0F");
+                console.log("Wind Speed: " + data.current.wind_speed);
+                console.log("Wind Chill: " + data.current.feels_like + " \xB0F");
+                console.log("Chance of Precipitation: " + data.hourly[0].pop);
+            });
+        }
+        else {
+            alert("Error: " + response.statusText);
+        }
+    })
+    .catch(function(error){
+        alert("Unable to connect to Open Weather. - " + error);
+    });
+}
+
 getTransitApi();
+
+findCity("Minneapolis");
